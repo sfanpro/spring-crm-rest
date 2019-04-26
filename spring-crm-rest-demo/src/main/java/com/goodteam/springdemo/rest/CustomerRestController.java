3ab28c3cef67dd4b82e1f4aae71a20c7661f9e65
sfanpro <st.sergeyf@gmail.com>
2019-04-26 20:42:34 +0300

    added new customer to DB using REST API

--- spring-crm-rest-demo/src/main/java/com/goodteam/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/goodteam/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +33,15 @@ public class CustomerRestController {
 		if (theCustomer == null) {
 			throw new CustomerNotFoundException("Customer id not found - " + custumerId);
 		}
+		return theCustomer;
+	}
+
+	// add mapping for POST /customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		// to save a new item instead update - set id to 0
+		theCustomer.setId(0);
+		customerService.saveCustomer(theCustomer);
 		return theCustomer;
 	}
 }
